af775eda27b75a0ae8ec31ce0f27604e9744da88
Richard Seward <raseward14@gmail.com>
2021-02-05 06:17:46 -0700

    added UV Index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,6 @@
 // - Your API key is 0f9dd32babe408905fb98d5398ec6131
 // - Within the next couple of hours, it will be activated and ready to use
-// - Please, always use your API key in each API call
-
-// Endpoint:
 // - Please, use the endpoint api.openweathermap.org for your API calls
-// - Example of API call:
-// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0f9dd32babe408905fb98d5398ec6131
-
 // Useful links:
 // - API documentation https://openweathermap.org/api
 
@@ -20,14 +14,19 @@ $(document).ready(function () {
     function getApi(searchTerm) {
 
         // var searchTerm = userInput.value;
-        if (searchTerm === 'undefined') {
-          searchTerm = userInput.value;
+        if (typeof searchTerm !== 'string') {
+            searchTerm = userInput.value;
         }
 
+        // after we have the search term, clear search value
         userInput.value = '';
+
+        // save my search in local storage
         storeCities(searchTerm);
         // for each click, create list item for each city, append to the city list
         var currentDay = document.getElementById('current-day');
+        currentDay.innerHTML = [];
+
         var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + searchTerm + '&units=imperial&appid=0f9dd32babe408905fb98d5398ec6131';
 
 
@@ -42,7 +41,6 @@ $(document).ready(function () {
 
                 console.log(data);
                 // and append the city name, date, temp, humidity, widnspeed, uvindex to the dashboard container
-
                 var cityName = document.createElement('h2');
                 cityName.textContent = data.city.name;
                 currentDay.appendChild(cityName);
@@ -50,8 +48,8 @@ $(document).ready(function () {
                 date.textContent = data.list[0].dt_txt;
                 cityName.append(date);
 
-                // var icon = document.createElement('symbol');
-                // icon = data.list[0].weather.icon.value;
+                // var icon = document.createElement('img');
+                // icon.innerHTML = data.list[0].weather.icon;
                 // currentDay.appendChild(icon);
 
                 var temperature = document.createElement('p');
@@ -71,9 +69,9 @@ $(document).ready(function () {
 
 
                 // 5 days, 1 day is 24 hours, 3 hour increments, 8 increments is 1 day, 40 increments is 5 days, if it can be divided by 8, its a day i want to print to the page
-                var dayForecast = document.getElementById('day-forecast')
-
-                for (let i = 8; i < data.list.length; i+=8) {
+                var dayForecast = document.getElementById('day-forecast');
+                dayForecast.innerHTML = [];
+                for (let i = 8; i < data.list.length; i += 8) {
                     var day = document.createElement('div');
                     day.classList.add('forecast');
                     dayForecast.appendChild(day);
@@ -90,31 +88,70 @@ $(document).ready(function () {
                     day.appendChild(forecastTemp);
                     day.appendChild(forecastHumidity);
                 }
+
                 renderHistory()
+
+                var lat = data.city.coord.lat;
+                console.log(data.city.coord.lat);
+                var lon = data.city.coord.lon;
+                console.log(data.city.coord.lon);
+
+                // get uvi, create p element, append to current day
+                var requestOptions = {
+                    method: 'GET',
+                    redirect: 'follow'
+                };
+
+                fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,daily,alerts&appid=0f9dd32babe408905fb98d5398ec6131", requestOptions)
+                    .then(response => { return response.json() })
+                    .then(result => {
+                        console.log(result);
+                        console.log(result.current.uvi);
+                        console.log(result.lat);
+                        console.log(result.lon);
+
+                        var UVI = document.createElement('p');
+                        UVI.textContent = ('UV Index: ' + result.current.uvi + '%');
+                        currentDay.appendChild(UVI);
+
+                    })
+                    .catch(error => { console.log('error', error) });
             })
     }
 
+
+    // set search terms to local storage
     function storeCities(searchTerm) {
+        // the cities in local storage are a string, get them
         var cities = getCities();
+        // now that its an array, push searchTerm on, and stringify cities array in local storage
         cities.push(searchTerm);
         localStorage.setItem('searchHistory', JSON.stringify(cities));
     }
 
+    // refresh saves the last city that she looked up, will be displayed on page
     function getCities() {
+        // get key searchHistory from local storage
         var cities = localStorage.getItem('searchHistory');
+        // if cities exist, json parse into array to be displayed
         if (cities !== null) {
             cities = JSON.parse(cities);
-        } else {
+        }
+        // else cities array is empty
+        else {
             cities = [];
         }
+        // returns cities array
         return cities;
     }
 
     function renderHistory() {
-
+        // get cities array
         var cities = getCities();
+        // set the userlist inner html to empty
         var userList = document.querySelector('ul');
         userList.innerHTML = null;
+        // for each city in the array, create a list item, text content is the text of the array, on click, run a function to add city name to url
         for (let i = 0; i < cities.length; i++) {
             var cityName = document.createElement('li');
             cityName.textContent = cities[i];
@@ -126,16 +163,16 @@ $(document).ready(function () {
     }
 
     function getButtonValue() {
+        // searchterm from url is this buttons text
         var searchTerm = this.textContent;
-        console.log(searchTerm);
+        console.log(typeof searchTerm);
+        // get api data with searchterm
         getApi(searchTerm);
     }
 
+    // on load call the citie in local storage
     renderHistory();
-    // api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt=6&appid=0f9dd32babe408905fb98d5398ec6131
 
-    // add event listener search button 'click' getApi(requestUrl) function
+    // on click, get the api
     searchBtn.addEventListener('click', getApi);
-    // local storage, dont need to push cities to an array
-    // refresh saves the last city that she looked up, will be displayed on page
 });
